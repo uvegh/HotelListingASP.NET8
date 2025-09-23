0ea08ea6e3cab7430e713103a820208d969e0987
uevgh <uveghobamien@gmail.com>
2025-09-23 12:57:41 +0100

    #db model setup
    Add Country and Hotel classes with database seeding
    
    Introduced a new `Country` class with properties for `Id`, `Name`, `ShortName`, and a collection of `Hotels`. Created a `Hotel` class with properties including `Id`, `Name`, `Address`, `Rating`, `CountryId`, and an optional `PostCode`, establishing a foreign key relationship with `Country`. Updated `HotelDBContext` to include `DbSet` properties for both entities and overridden `OnModelCreating` to

---
+++ HotelListing/Data/Hotel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelListing.Data
+{
+    public class Hotel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Rating { get; set; }
+
+        [ForeignKey(nameof (CountryId))]
+        public int CountryId { get; set; }
+        public Country Country { get; set; }
+
+        [MaxLength(9,ErrorMessage ="Post code cannot be more than 9 characters")]
+        public string? PostCode { get; set; }
+
+
+    }
+}
---
+++ HotelListing/Data/HotelDBContext.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.Data
+{
+    public class HotelDBContext:DbContext
+    {
+        public HotelDBContext(DbContextOptions<HotelDBContext> options ):base(options) { 
+        
+        }
+
+      public DbSet<Hotel> Hotels { get; set; }
+      public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name="Nigeria",
+                    ShortName="NG"
+                },
+                new Country
+                {
+                    Id=2,
+                    Name="United Kingdom",
+                    ShortName="UK"
+                },
+                new Country
+                {
+                    Id=3,
+                    Name="United States of America",
+                    ShortName="USA"
+                }
+              
+                );
+
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Address = "Lagos,Banana Island",
+                    CountryId = 1,
+                    Name = "Eko hotel",
+                    Rating = 3.5
+                },
+                new Hotel
+                {
+                    Id=2,
+                    Address="Faldon Lodge ",
+                    CountryId=2,
+                    PostCode="B1,8BQ",
+                    Name="Falcon Central Hotels",
+                    Rating=4
+                }
+                );
+        }
+    }
+}
