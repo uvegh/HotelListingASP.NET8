d64f76c2602bfeb6d4c3772ed385acb16382f37f
uevgh <uveghobamien@gmail.com>
2025-09-27 14:46:24 +0100

    Add AutoMapper configuration for DTOs and models
    This commit introduces a new `MapperConfig` class in the
    `HotelListing.Configurations` namespace. It sets up
    bidirectional mappings between various DTOs, including
    `CreateCountryDto`, `EditCountryDto`, `GetCountryDto`,
    `CountryDto`, `HotelDto`, and `BaseCountryDto`, and their
    respective model classes, facilitating seamless data
    transformation within the application.

---
+++ HotelListing/Configurations/MapperConfig.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using HotelListing.Data;
+using HotelListing.model.Country;
+using HotelListing.Models.Country;
+using HotelListing.Models.Hotel;
+
+namespace HotelListing.Configurations
+{
+    public class MapperConfig:Profile
+    {
+
+        public MapperConfig()
+        {
+            CreateMap<CreateCountryDto, Country>().ReverseMap();
+            CreateMap<EditCountryDto, Country>().ReverseMap();
+            CreateMap<GetCountryDto, Country>().ReverseMap();
+            CreateMap<CountryDto, Country>().ReverseMap();
+            CreateMap<HotelDto, Hotel>().ReverseMap();
+            CreateMap<BaseCountryDto, Country>().ReverseMap();
+        }
+    }
+}
