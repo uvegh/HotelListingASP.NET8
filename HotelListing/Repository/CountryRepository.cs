096d64bc0484230125bc4821ef9462b0e659a191
uevgh <uveghobamien@gmail.com>
2025-09-28 22:23:21 +0100

    Add CountryRepository and enhance GenericRepository
    Implemented CountryRepository class for country management.
    Enhanced GenericRepository with CRUD methods: CreateAsync,
    DeleteAsync, GetAllAsync, Exists, GetAsync, and UpdateAsync.
    Updated using directives for necessary namespaces.

---
+++ HotelListing/Repository/CountryRepository.cs
@@ -0,0 +1,15 @@
+﻿using HotelListing.Contracts;
+using HotelListing.Data;
+
+namespace HotelListing.Repository
+{
+    public class CountryRepository:GenericRepository<Country>,ICountryRepository
+    {
+        private readonly HotelDBContext _context;
+
+
+        public CountryRepository(HotelDBContext context):base(context) {
+            _context = context;
+        }
+    }
+}
---
+++ HotelListing/Repository/GenericRepository.cs
@@ -0,0 +1,67 @@
+﻿using HotelListing.Contracts;
+using HotelListing.Data;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class {
+
+        private readonly HotelDBContext _context;
+
+        public GenericRepository( HotelDBContext context) {
+            _context = context;
+        }
+        public async Task<T> CreateAsync(T entity) {
+            await _context.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+         
+        }
+
+        public async Task DeleteAsync(int id) {
+            var entity = await GetAsync(id);
+            if (entity is not  null) 
+            _context.Remove(entity);
+            await _context.SaveChangesAsync();
+            
+
+        }
+
+        public async Task <List<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+
+        }
+         
+        public async Task <bool> Exists(int id)
+        {
+            var entity = await _context.Set<T>().FindAsync(id);
+            return entity is not null;                                          
+
+        }
+        public async Task <T> GetAsync(int id)
+
+        {
+            var entity = await _context.Set<T>().FindAsync(id);
+            if(entity is null) throw new Exception($"Entity of type {typeof(T).Name} with id {id} not found"); 
+            return entity;
+
+           
+
+        }
+        public async Task <T?> UpdateAsync(int id, T Entity)
+        {
+            if (Exists(id) is null) return null;
+         var obj=  await _context.Set<T>().FindAsync(id);
+            if (obj is null) return null;
+            _context.Set<T>().Update(Entity);
+            await _context.SaveChangesAsync();
+            return Entity;
+        }
+
+
+
+
+    }
+}
