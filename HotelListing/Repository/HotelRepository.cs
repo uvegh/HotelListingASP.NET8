af7373bcf33a3b34ee21b03ce06592be71d45e7b
uevgh <uveghobamien@gmail.com>
2025-10-01 02:57:15 +0100

    Add HotelRepository with GetDetails method
    Introduces a new HotelRepository class implementing the IHotelRepository interface.
    The repository is initialized with HotelDBContext and a logger.
    A GetDetails method is added to retrieve hotel information by ID, including associated country data using Entity Framework's Include method.

---
+++ HotelListing/Repository/HotelRepository.cs
@@ -0,0 +1,31 @@
+﻿using HotelListing.Contracts;
+using HotelListing.Data;
+using HotelListing.Models.Hotel;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.Repository
+{
+    public class HotelRepository:GenericRepository<Hotel>,IHotelRepository
+    {
+
+        private readonly HotelDBContext _context;
+        private readonly ILogger<HotelRepository> _logger;
+       public HotelRepository(HotelDBContext context, ILogger<HotelRepository> logger):base(context)
+        {
+            _context = context;
+            _logger = logger;
+            
+        }
+
+        public async    Task<Hotel?> GetDetails(int id)
+        {
+            var hotel = await _context.Hotels.Include(q => q.Country).FirstOrDefaultAsync(res => res.Id==id);
+            
+            return hotel;
+
+
+        }
+
+       
+    }
+}
