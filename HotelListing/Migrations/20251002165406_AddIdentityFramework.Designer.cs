c77063979859fbf9cfe98c00f570fd9664b40b52
uevgh <uveghobamien@gmail.com>
2025-10-03 14:24:14 +0100

    Add identity framework support to HotelListing
    This commit introduces user and role management by adding
    the necessary tables (`AspNetUsers`, `AspNetRoles`, etc.)
    to the database. Migration files define the schema and
    initial role data (Administrator and User). The model
    snapshot is updated to reflect these changes, ensuring
    Entity Framework Core is in sync with the database schema.
    The `BuildTargetModel` method in migration designer files
    has also been updated to include the new entities and
    their relationships.

--- HotelListing/Migrations/20251002165406_AddIdentityFramework.Designer.cs
+++ HotelListing/Migrations/20251002165406_AddIdentityFramework.Designer.cs
@@ -4,6 +4,7 @@ using HotelListing.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HotelListing.Migrations
 {
     [DbContext(typeof(HotelDBContext))]
-    partial class HotelDBContextModelSnapshot : ModelSnapshot
+    [Migration("20251002165406_AddIdentityFramework")]
+    partial class AddIdentityFramework
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -213,20 +216,6 @@ namespace HotelListing.Migrations
                         .HasFilter("[NormalizedName] IS NOT NULL");
 
                     b.ToTable("AspNetRoles", (string)null);
-
-                    b.HasData(
-                        new
-                        {
-                            Id = "1",
-                            Name = "Administrator",
-                            NormalizedName = "ADMINISTRATOR"
-                        },
-                        new
-                        {
-                            Id = "2",
-                            Name = "User",
-                            NormalizedName = "USER"
-                        });
                 });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRoleClaim<string>", b =>
