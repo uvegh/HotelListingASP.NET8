955840ff232b76907608ccb2385d8cd0c2bafa7d
uevgh <uveghobamien@gmail.com>
2025-10-05 05:23:26 +0100

    edit progrma.cs file

--- HotelListing/Program.cs
+++ HotelListing/Program.cs
@@ -1,16 +1,23 @@
 using HotelListing.Configurations;
 using HotelListing.Contracts;
+using HotelListing.Contracts.User;
 using HotelListing.Data;
 using HotelListing.Repository;
+
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectString = builder.Configuration.GetConnectionString("DbConnectionString");
-builder.Services.AddDbContext<HotelDBContext>(options => options.UseSqlServer(connectString));
+//builder.Services.AddDbContext<HotelDBContext>(options => options.UseSqlServer(connectString));
+
+builder.Services.AddDbContext<HotelDBContext>(options =>
+    options.UseSqlServer(connectString,
+        b => b.MigrationsAssembly("HotelListing")));
+builder.Services.AddIdentityCore<ApiUser>().AddRoles<IdentityRole>().AddEntityFrameworkStores<HotelDBContext>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -23,7 +30,7 @@ AllowAnyMethod().
 AllowAnyOrigin());
 });
 builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
-builder.Services.AddAutoMapper(cfg=>cfg.AddProfile<MapperConfig>() );
+builder.Services.AddAutoMapper(cfg => cfg.AddProfile<MapperConfig>());
 // Replace this line:
 // builder.Services.AddAutoMapper(typeof(MapperConfig));
 // With this line: add the type,the implementation no need to specify the type explicitly
@@ -34,6 +41,7 @@ builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepositor
 //Add the type/ interfacr and implementation of repository
 builder.Services.AddScoped<ICountryRepository, CountryRepository>();
 builder.Services.AddScoped<IHotelRepository, HotelRepository>();
+builder.Services.AddScoped<IAuthRepository, AuthRepository>();
 
 var app = builder.Build();
 
@@ -46,6 +54,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
