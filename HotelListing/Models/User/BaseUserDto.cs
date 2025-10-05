bbb80cd70bab3f1c970508ef93b9134a77085249
uevgh <uveghobamien@gmail.com>
2025-10-05 05:21:25 +0100

    Add Signup and Login dto

---
+++ HotelListing/Models/User/BaseUserDto.cs
@@ -0,0 +1,18 @@
+﻿using HotelListing.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.Models.User
+{
+    public class BaseUserDto
+    {
+
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+    }
+}
---
+++ HotelListing/Models/User/SignupDto.cs
@@ -0,0 +1,24 @@
+﻿using HotelListing.Data;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.Models.User
+{
+    public class SignupDto:BaseUserDto
+    {
+       
+
+        [PasswordPropertyText]
+        public string Password { get; set; }
+    }
+
+    public class SignupResultDto
+    {
+        public IEnumerable<IdentityError> Errors= Enumerable.Empty<IdentityError>();
+
+        public ApiUser? User  { get; set; }
+
+        public bool Success => !Errors.Any() && User != null;
+    }
+}
