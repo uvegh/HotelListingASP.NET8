dc644601f66fa99db1f7e3598a921f30172b49bc
uevgh <uveghobamien@gmail.com>
2025-10-05 05:22:08 +0100

    Add auth repo interface

---
+++ HotelListing/Contracts/User/IAuthRepository.cs
@@ -0,0 +1,13 @@
+﻿using HotelListing.Data;
+using HotelListing.Models.User;
+using Microsoft.AspNetCore.Identity;
+
+namespace HotelListing.Contracts.User
+{
+    public interface IAuthRepository
+    {
+
+        Task <IEnumerable<IdentityError>> Signup(SignupDto signupDto);
+        Task<bool> Login(LoginDto loginDto);
+    }
+}
