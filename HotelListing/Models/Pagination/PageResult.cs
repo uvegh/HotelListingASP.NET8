7683a033f0866ddde334078231cf1c6b22595ab9
uevgh <uveghobamien@gmail.com>
2025-10-12 23:18:27 +0100

    Add pagination classes for improved data handling
    Introduces `PageResult<T>` for managing paginated results
    and `QueryParameters` for handling pagination parameters
    with a default page size of 10. The `PageSize` property
    now utilizes a private backing field for better encapsulation.

---
+++ HotelListing/Models/Pagination/PageResult.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelListing.Models.Pagination
+{
+    public class PageResult<T>
+    {
+        public int CurrentPage { get; set; }
+       public int TotalPages { get; set; }
+        public int TotalCount { get; set; }
+        public int RecordNumber { get; set; }
+        public List <T> Items { get; set; }
+    }
+}
---
+++ HotelListing/Models/Pagination/QueryParameters.cs
@@ -0,0 +1,20 @@
+﻿namespace HotelListing.Models.NewFolder
+{
+    public class QueryParameters
+    {
+        private int _pagesize = 10;
+
+        public int StartIndex { get; set; }
+
+        public int PageSize { 
+        
+        //get { return _pagesize; }
+        get=> _pagesize;
+
+        set => _pagesize = value;
+
+        }
+
+
+    }
+}
