a2917b74e761a4500cef77294476207be432b97f
uevgh <uveghobamien@gmail.com>
2025-10-12 23:19:18 +0100

    Refactor IGenericRepository for pagination support
    Removed namespace declaration and added using directives for
    new models. Updated GetAllAsync method to return
    Task<PageResult<TResult>> with QueryParameters for
    pagination instead of a simple list.

--- HotelListing/Contracts/IGenericRepository.cs
+++ HotelListing/Contracts/IGenericRepository.cs
@@ -1,9 +1,12 @@
-﻿namespace HotelListing.Contracts
+﻿using HotelListing.Models.NewFolder;
+using HotelListing.Models.Pagination;
+
+namespace HotelListing.Contracts
 {
     public interface IGenericRepository<T> where T:class
     {
         Task<T> GetAsync(int id);
-        Task<List<T>> GetAllAsync();
+        Task<PageResult <TResult>> GetAllAsync<TResult>(QueryParameters queryParameters);
         Task<T> CreateAsync(T entity);
         Task<T?> UpdateAsync( T entity);
         Task DeleteAsync(int id);
