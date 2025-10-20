5e6bc5a45ae18d8ebdf6f4fdc6c1ed47552592e7
uevgh <uveghobamien@gmail.com>
2025-10-20 15:21:41 +0100

    Enhance IGenericRepository with mapping methods
    Updated the `IGenericRepository.cs` interface in the `HotelListing.Contracts` namespace to include new methods for entity mapping and updating. Added `CreateMapAsync<TSource, TResult>(TSource entity)` for creating entities with mapping, and introduced `UpdateAsyncMap<TSource, TResult>(int id, TSource source)` for updating entities based on a source type. The existing `DeleteAsync(int id)` method remains unchanged.

--- HotelListing.Core/Contracts/IGenericRepository.cs
+++ HotelListing.Core/Contracts/IGenericRepository.cs
@@ -9,8 +9,13 @@ namespace HotelListing.Contracts
         Task<PageResult <TResult>> GetAllPagedAsync<TResult>(QueryParameters queryParameters);
         Task<List<T>> GetAllAsync();
         Task<T> CreateAsync(T entity);
+        Task<TResult> CreateMapAsync <TSource,TResult>(TSource entity);
         Task<T?> UpdateAsync( T entity);
-        Task DeleteAsync(int id);
+        Task<TResult?> UpdateAsyncMap<TSource, TResult>(int id, TSource source)
+             where TResult : class
+             where TSource : class;
+   
+           Task DeleteAsync(int id);
         Task<bool> Exists(int id);
 
     }
