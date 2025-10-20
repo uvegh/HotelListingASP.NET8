527d2532192802c9685a8b3c983b5e1d01defab7
uevgh <uveghobamien@gmail.com>
2025-10-20 15:20:47 +0100

    Enhance GenericRepository with AutoMapper integration
    Added methods for entity mapping using AutoMapper, including
    CreateMapAsync and UpdateAsyncMap. Improved method formatting
    for consistency and readability. Adjusted Exists and GetAsync
    methods for clarity. Overall, these changes enhance the
    functionality and maintainability of the GenericRepository class.

--- HotelListing.Core/Repository/GenericRepository.cs
+++ HotelListing.Core/Repository/GenericRepository.cs
@@ -5,33 +5,47 @@ using HotelListing.Data;
 using HotelListing.Models.Country;
 using HotelListing.Models.NewFolder;
 using HotelListing.Models.Pagination;
-
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
 
 namespace HotelListing.Repository
 {
-    public class GenericRepository<T> : IGenericRepository<T> where T : class {
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
 
         private readonly HotelDBContext _context;
-        private readonly IMapper _mapper;                                   
+        private readonly IMapper _mapper;
 
-        public GenericRepository( HotelDBContext context, IMapper mapper) {
+        public GenericRepository(HotelDBContext context, IMapper mapper)
+        {
             _context = context;
             _mapper = mapper;
         }
-        public async Task<T> CreateAsync(T entity) {
+        public async Task<T> CreateAsync(T entity)
+        {
             await _context.AddAsync(entity);
             await _context.SaveChangesAsync();
             return entity;
-         
+
         }
 
-        public async Task DeleteAsync(int id) {
-            var entity = await GetAsync(id);
-            if (entity is not  null) 
-            _context.Remove(entity);
+        public async Task<TResult> CreateMapAsync<TSource, TResult>(TSource Entity)
+        {
+            //map source entity to destination entity
+            var res = _mapper.Map<T>(Entity);
+            await _context.AddAsync(res);
             await _context.SaveChangesAsync();
-            
+            return _mapper.Map<TResult>(Entity);
+
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await GetAsync(id);
+            if (entity is not null)
+                _context.Remove(entity);
+            await _context.SaveChangesAsync();
+
 
         }
 
@@ -40,7 +54,7 @@ namespace HotelListing.Repository
             return await _context.Set<T>().ToListAsync();
 
         }
-        public async Task<PageResult <TResult>> GetAllPagedAsync <TResult>(QueryParameters queryParameters)
+        public async Task<PageResult<TResult>> GetAllPagedAsync<TResult>(QueryParameters queryParameters)
         {
             //get total count of records in the table
             var totalSize = await _context.Set<T>().CountAsync();
@@ -50,15 +64,15 @@ namespace HotelListing.Repository
 //add mapper config so that it can be mapped to dto
 (queryParameters.PageSize).ProjectTo<TResult>(_mapper.ConfigurationProvider).ToListAsync();
 
-            return  new PageResult<TResult>
+            return new PageResult<TResult>
             {
-                Items=items,
+                Items = items,
                 //total count of all items
-                TotalCount=totalSize,
+                TotalCount = totalSize,
                 //current number of items on pages
-                RecordNumber=items.Count,
-           
-//currwnt page
+                RecordNumber = items.Count,
+
+                //currwnt page
                 CurrentPage = queryParameters.PageNumber,
             };
         }
@@ -73,15 +87,15 @@ namespace HotelListing.Repository
         //        .ProjectTo<GetCountryDto>(_mapper.ConfigurationProvider);
         //}
 
-       
 
-        public async Task <bool> Exists(int id)
+
+        public async Task<bool> Exists(int id)
         {
             var entity = await _context.Set<T>().FindAsync(id);
-            return entity is not null;                                          
+            return entity is not null;
 
         }
-        public async Task <T> GetAsync(int id)
+        public async Task<T> GetAsync(int id)
 
         {
             var entity = await _context.Set<T>().FindAsync(id);
@@ -89,21 +103,47 @@ namespace HotelListing.Repository
             //if (entity is null) return null;
             return entity;
 
-           
+
 
         }
-        public async Task <T?> UpdateAsync( T Entity)
+        public async Task<T?> UpdateAsync(T Entity)
         {
             //if (Exists(id) is null) return null;
-         //var obj=  await _context.Set<T>().FindAsync(id);
-         //   if (obj is null) return null;
+            //var obj=  await _context.Set<T>().FindAsync(id);
+            //   if (obj is null) return null;
             _context.Set<T>().Update(Entity);
             await _context.SaveChangesAsync();
             return Entity;
         }
 
+        //public async Task<T?> UpdateAsyncMap<TSource,TResult>(TSource entity,TResult res)
+        //where TResult:class
+        //    where TSource : class
+        //{
+
+        //    _mapper.Map(entity,res);
+
+        //    _context.Set<T>().Update(res);
+        //    await _context.SaveChangesAsync();
 
 
+        //}
 
+
+        public async Task<TResult?> UpdateAsyncMap<TSource, TResult>(int id, TSource source)
+          where TResult : class
+          where TSource : class
+        {
+            var entity = await _context.Set<T>().FindAsync(id);
+            if (entity == null) return null;
+
+         var result=   _mapper.Map(source, entity);
+            _context.Update(entity);
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<TResult> (entity);
+        }
+
+       
     }
 }
